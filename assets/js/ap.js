76e74f4edfdd88da40f80c2386f1485225502d57
Andrew Robinson <arobinson1289@gmail.com>
2019-04-09 14:47:42 -0400

    changes

--- assets/js/ap.js
+++ assets/js/ap.js
@@ -3,7 +3,55 @@ $(document).ready(function(){
 
 
     // Create Var To Deploy Once Search Imput Button Is Clicked
+    var searchArray = ["dog","cats","birds"]
+    
+    function displaybuttons (){
+        
+        for (var i = 0; i < searchArray.length; i++) {
+            var buttonDiv = $("<button>")
+            buttonDiv.addClass("btn btn-primary arrayButton");
+            buttonDiv.text(searchArray[i]);
+            buttonDiv.attr("value", searchArray[i])
+
+            $("#buttonContainer").append(buttonDiv);
+        }
+    }
+    
+    displaybuttons();
+
+    $(".arrayButton").on("click", function(){
+        $("#images").empty();
+        var searchedTerm = $(this).val();
+
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchedTerm + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+        //Perform AJAX QUERY
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+
+        //After Data Returns From API
+        .then(function(response){
+            var results = response.data;
+
+            for (var i=0; i < results.length; i++) {
+
+                var img = $("<img>");
+                img.addClass("imageBtn");
+                img.attr("src", results[i].images.fixed_height_small_still.url);
+                img.attr("data-still", results[i].images.fixed_height_small_still.url);
+                img.attr("data-animate", results[i].images.fixed_height_small.url);
+
+
+                $("#images").append(img);
+            }
+
+        });
+    });
+
     $("#searchButton").on("click", function(){
+        $("#images").empty();
         var searchedTerm = $("#input").val();
 
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchedTerm + "&api_key=dc6zaTOxFJmzC&limit=10";
@@ -29,6 +77,13 @@ $(document).ready(function(){
 
                 $("#images").append(img);
             }
+
+            searchArray.push(searchedTerm);
+
+            $("#buttonContainer").empty();
+
+            displaybuttons();
+
 
         });
     });
